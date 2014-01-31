696e75a41be39a37e26a71e12f74cf12abdd4ad8
Jonatan Soulieres <jonatan_soulieres@hotmail.com>
2014-01-30 23:20:27 -0300

    Ajout d'un SoP

--- src/javaapplicationinidepot/JavaApplicationIniDepot.java
+++ src/javaapplicationinidepot/JavaApplicationIniDepot.java
@@ -1,23 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package javaapplicationinidepot;
 
-/**
- *
- * @author gh191177
- */
 public class JavaApplicationIniDepot {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-    int i = 0 ;
+        int i = 0 ;
+        System.out.println("Voici un projet qui est configuré avec GitHub");
     }
     
 }
